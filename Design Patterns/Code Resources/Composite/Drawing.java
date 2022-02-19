e1176bf7bc4b247d8ececa50754d0380e7d6a275
Divyansh Verma <divyanshverma12@gmail.com>
2022-02-19 19:48:55 +0530

    Composite Pattern

---
+++ Design Patterns/Code Resources/Composite/Drawing.java
@@ -0,0 +1,27 @@
+package Composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Drawing implements Shape {
+    private List<Shape> shapes = new ArrayList<Shape>();
+
+    @Override
+    public void draw(String fillColor) {
+        for (Shape sh: shapes) {
+            sh.draw(fillColor);
+        }
+    }
+
+    public void add(Shape s) {
+        shapes.add(s);
+    }
+
+    public void remove(Shape s) {
+        shapes.remove(s);
+    }
+
+    public void clear() {
+        shapes.clear();
+    }
+}
